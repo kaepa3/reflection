4c8bc408a538defdca95fa987fc77d3f91fce725
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2021-05-04 20:21:29 +0900

    end 1.2'

--- 1.2.1/main.go
+++ 1.2.1/main.go
@@ -5,19 +5,23 @@ import (
 	"reflect"
 )
 
-func isInt(v interface{}) bool {
-	rt := reflect.TypeOf(v)
-	switch rt.Kind() {
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return true
-	default:
-		return false
+func plus(v1, v2 interface{}) interface{} {
+	rv1, rv2 := reflect.ValueOf(v1), reflect.ValueOf(v2)
+	if isInt(rv1.Kind()) && isInt(rv2.Kind()) {
+		return interface{}(rv1.Int() + rv2.Int())
+	} else if rv1.Kind() == reflect.String && rv2.Kind() == reflect.String {
+		return interface{}(rv1.String() + rv2.String())
 	}
+
+	return nil
+}
+func isInt(k reflect.Kind) bool {
+	return k == reflect.Int || k == reflect.Int8 || k == reflect.Int16 || k == reflect.Int32 || k == reflect.Int64
 
 }
 func main() {
-	fmt.Println(isInt(1))
-	fmt.Println(isInt("1"))
-	fmt.Println(isInt(false))
+	fmt.Println(plus(1, 20))
+	fmt.Println(plus("1", "fsda"))
+	fmt.Println(plus(false, 20))
 
 }
